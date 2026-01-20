0fd77011fb1ca1236f71f6813a9d528a1692a75a
jhfated6 <jh.cristo06@hotmail.com>
2026-01-20 00:05:24 -0300

    Create Protocol.java

---
+++ src/common/Protocol.java
@@ -0,0 +1,14 @@
+package common;
+
+public final class Protocol {
+    private Protocol() {}
+
+    public static final String NICK = "NICK:";
+    public static final String MSG  = "MSG:";
+    public static final String PM   = "PM:";
+    public static final String JOIN = "JOIN:";
+    public static final String WHO  = "WHO";
+    public static final String QUIT = "QUIT";
+    public static final String OK   = "OK:";
+    public static final String ERR  = "ERR:";
+}
